5851ca0f12b0075993f04628a4f777b9091f9ae4
Fidel Salazar <fidelsf91@gmail.com>
2023-01-27 18:34:14 -0600

    POST call to api page

--- src/pages/PostEmployee.js
+++ src/pages/PostEmployee.js
@@ -1,7 +1,43 @@
+import { CLIENT_PUBLIC_FILES_PATH } from "next/dist/shared/lib/constants";
+
 export default function PostEmployee() {
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const data = {
+            id: event.target.id.value,
+            name: event.target.name.value, 
+        }
+
+        const JSONdata = JSON.stringify(data);
+
+        const endpoint = "http://localhost:5288/api/Employees/";
+
+        console.log(JSONdata);
+
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSONdata,
+        }
+
+        const response = await fetch(endpoint, options);
+
+        const result = await response.json();
+    }
     return (
         <>
-            <h1>Page 2</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="id">id:</label>
+                <input type="number" id="id" name="id" required minLength="1"/>
+
+                <label htmlFor="name">name:</label>
+                <input type="name" id="name" name="name" required minLength="2" />
+
+                <button type="submit">Submit</button>
+            </form>
         </>
     );
 }
